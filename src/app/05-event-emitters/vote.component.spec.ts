d5cbdc7677eec952ba711bddab0b0ba7d322a186
danieldelajustina <dd.justina@hotmail.com>
2019-11-10 22:25:38 -0300

    Tests event emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,16 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  let component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes = null;
+    component.voteChanged.subscribe(tot => totalVotes = tot);
+    component.upVote();
+    expect(totalVotes).toBe(1);
   });
 });
